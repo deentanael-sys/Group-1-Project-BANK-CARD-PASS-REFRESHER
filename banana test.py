72732a267dd099fd2fe1356f3b93d23b31f1a141
Deed <deentanael@gmail.com>
2025-11-26 14:32:26 +0300

    git pull origin main --no-rebase

--- banana test.py
+++ banana test.py
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 from banana import generate_password
 from banana import password
 
 
 
 if password():
-=======
-from banana import generate_password
-from THEPASS import password
-
-
-
-if password():
->>>>>>> bc5a14655b31c2beae289bc58fa177219fc9ae1d
     generate_password()
+else:
+    print("Access Denied")
--- banana.py
+++ banana.py
@@ -2,14 +2,9 @@ def generate_password():
     print("password")
     print("password")
   
-<<<<<<< HEAD
 def password():
     p = "128132"
     if p == "128132":
         return p
     else:
         return None
-
-    
-=======
->>>>>>> bc5a14655b31c2beae289bc58fa177219fc9ae1d
